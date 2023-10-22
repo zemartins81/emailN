3fa120caf82a985c7bf3cd2cd07dc2844fe6dee4
José C. Martins <zemartins81@gmail.com>
2023-10-22 11:31:10 -0300

    test: test create new Campaign

--- internal/domain/campaign/campaign_test.go
+++ internal/domain/campaign/campaign_test.go
@@ -3,5 +3,25 @@ package campaign
 import "testing"
 
 func TestNewCampaign(t *testing.T) {
+	name := "Campaign X"
+	content := "Body"
+	contacts := []string{"email@email.com", "email2@email.com"}
 
+	campaign := NewCampaign(name, content, contacts)
+
+	if campaign.ID != "1" {
+		t.Errorf("expected 1")
+	}
+
+	if campaign.Name != name {
+		t.Errorf("expected correct name")
+	}
+
+	if campaign.Content != content {
+		t.Errorf("expected correct content")
+	}
+
+	if len(campaign.Contacts) != len(contacts) {
+		t.Errorf("expected correct contacts")
+	}
 }
