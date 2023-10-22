0108b1fc9bcf220ffca1cfd2269a9270f45b688c
José C. Martins <zemartins81@gmail.com>
2023-10-22 11:01:52 -0300

    Initial commit

---
+++ internal/domain/campaign/campaigm.go
@@ -0,0 +1,15 @@
+package campaign
+
+import "time"
+
+type Contact struct {
+	Email string
+}
+
+type Campaign struct {
+	ID        string
+	Name      string
+	CreatedAt time.Time
+	Content   string
+	Contacts  []Contact
+}
